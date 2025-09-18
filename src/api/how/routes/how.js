76a3b62c04a47bd7027cc4a5c8c83e98501b54e7
Motzart <motzart66@gmail.com>
2025-09-18 12:37:03 +0300

    Remove 'how' content type, including its controller, router, and service files, to streamline the API structure.

--- src/api/how/routes/how.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * how router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::how.how');
--- src/api/how/services/how.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * about how.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::how.how');
